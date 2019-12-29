7bdeeb9303f90faf82e601e5fda236bd6392d24e
FerdinandIV <ferdinand1@live.co.za>
2019-12-29 12:31:44 +0200

    End of section 4

--- DatingApp.API/Controllers/ValuesController.cs
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -22,6 +22,7 @@ namespace DatingApp.API.Controllers
 
         }
         // GET api/values
+        [AllowAnonymous]
         [HttpGet]
         public async Task<IActionResult> GetValues() //IActionResult returns http 200 response OK
         {
